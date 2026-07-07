0945f1ba3ea657ffb8110428f09a1cf9c3bd4ae0
Farazdaq <sleekdxb@gmail.com>
2026-07-07 15:07:18 +0400

    Arrays Internally in Java  #17

--- Chapter3/DataType/NonPrimitive/Main.java
+++ Chapter3/DataType/NonPrimitive/Main.java
@@ -2,11 +2,13 @@ package Chapter3.DataType.NonPrimitive;
 
 public class Main {
     public static void main(String[] args){
-        StringDemo obj = new StringDemo();
+//        StringDemo obj = new StringDemo();
+//        obj.display();
+//        obj.getLength();
+//        obj.getSecondLetter();
+//        obj.getUpperCaseForm();
+//        obj.modifyString("I am the added text");
+        ArrayDemo obj = new ArrayDemo();
         obj.display();
-        obj.getLength();
-        obj.getSecondLetter();
-        obj.getUpperCaseForm();
-        obj.modifyString("I am the added text");
     }
 }
