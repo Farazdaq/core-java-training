1a992c5ab5748657374de086663331f9a0eda823
Farazdaq <frzdg6@gmail.com>
2026-07-20 13:51:17 +0400

    Java Documentation Comments (Javadoc) and Best Practices  #46

--- Chapter4/Main.java
+++ Chapter4/Main.java
@@ -6,16 +6,18 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        //MathClassDemo obj = new MathClassDemo();
+            // MathClassDemo obj = new MathClassDemo();
 
 
-       // ConstructorDemo emp = new ConstructorDemo("John", 50000);
+          // ConstructorDemo emp = new ConstructorDemo("John", 50000);
 
-      // DefaultFieldInitialization obj = new DefaultFieldInitialization();
+         // DefaultFieldInitialization obj = new DefaultFieldInitialization();
 
-       // Date today = new Date();
-        RecordDemo roj = new RecordDemo(145, "Farazdaq ahemd", "Software");
-        System.out.println(roj.getInfo());
+        // Date today = new Date();
+
+       // RecordDemo roj = new RecordDemo(145, "Farazdaq ahemd", "Software");
+
+       // System.out.println(roj.getInfo());
     
     }
 
